2b3fde4ae2885e9bea610c6c15edb8db94a0ee83
Joseph <borkowski.joseph@gmail.com>
2023-07-15 14:49:19 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.querySelector('#button');
+  
+    button.addEventListener('click', function() {
+      console.log('Button clicked');
+    });
+  
+    return true;
+  }
+  
+  addingEventListener();
